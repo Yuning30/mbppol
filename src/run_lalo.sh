1719a7be7f17a29562edd54360f3390cd6f3d030
Yuning30 <1987150762@qq.com>
2024-01-26 04:47:45 -0500

    lalo rst

--- src/run_lalo.sh
+++ src/run_lalo.sh
@@ -1,4 +1,4 @@
-timeout 30m python3.11 src/mbppo_lagrangian.py --env lalo --seed 0
-timeout 30m python3.11 src/mbppo_lagrangian.py --env lalo --seed 1
-timeout 30m python3.11 src/mbppo_lagrangian.py --env lalo --seed 2
-timeout 30m python3.11 src/mbppo_lagrangian.py --env lalo --seed 3
+timeout 45m python3.11 src/mbppo_lagrangian.py --env lalo --seed 0
+timeout 45m python3.11 src/mbppo_lagrangian.py --env lalo --seed 1
+timeout 45m python3.11 src/mbppo_lagrangian.py --env lalo --seed 2
+timeout 45m python3.11 src/mbppo_lagrangian.py --env lalo --seed 3
